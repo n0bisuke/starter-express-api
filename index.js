dd4b08d64dbb8cc5b0f58a8ba0ed4a0e9488107c
n0bisuke <rsksound@gmail.com>
2022-09-07 19:15:53 +0900

    new

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const http = require('http');
+const express = require("express");
+const RED = require("node-red");
+
+// Expressアプリケーションの生成
+const app = express();
+
+// 静的コンテンツのルートを追加
+app.use("/",express.static("public"));
+
+// サーバの生成
+const server = http.createServer(app);
+
+// 設定オブジェクトの生成 - 他のオプションについてはデフォルトの 'settings.js' ファイルを参照してください
+const settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"./.nodered",
+    functionGlobalContext: { }    // グローバルコンテキストを有効化
+};
+
+// サーバと設定とランタイムの初期化
+RED.init(server,settings);
+// エディタUIのルートを '/red' に指定
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// HTTP node UIのルートを '/api' に指定
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(process.env.PORT || 8000);
+
+// ランタイム起動
+RED.start();
